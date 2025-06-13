17c6bbcd4d2cddcfff7d8e87fa6575802238aa6f
Bryce <bteto3@gatech.edu>
2025-06-13 11:17:27 -0400

    Added water discharge data columns

--- Scripts/water_data_post_processing.py
+++ Scripts/water_data_post_processing.py
@@ -55,6 +55,7 @@ def water_data_post_processing():
                 dri_post_processing_df.loc[dri, "Contains 'data center'?"] = 1
             else:
                 dri_post_processing_df.loc[dri, "Contains 'data center'?"] = 0
+              
             try:
                 dri_post_processing_df.loc[dri, "Water Usage"] = project_info[13][1].lower()
                 if 'n/a' in project_info[13][1].lower():
@@ -81,10 +82,40 @@ def water_data_post_processing():
             except IndexError:
                 dri_post_processing_df.loc[dri, "Water Usage"] = "No water data"
                 dri_post_processing_df.loc[dri, "Cleaned Water Usage Data"] = "No water data"
+            dri_post_processing_df["Cleaned Water Discharge Data"] = dri_post_processing_df["Cleaned Water Discharge Data"].astype("object")
+            try:
+                dri_post_processing_df.loc[dri, "Water Discharge Data"] = project_info[17][1].lower()
+                if 'n/a' in project_info[17][1].lower():
+                    s = project_info[17][1].lower()
+                    dri_post_processing_df.loc[dri, "Water Discharge Data"] = "No water data"
+                    dri_post_processing_df.loc[dri, "Cleaned Water Discharge Data"] = "No water data"
+                elif 'mgd' in project_info[17][1].lower():
+                    s = project_info[17][1].lower()
+
+                    dri_post_processing_df.loc[dri, "Cleaned Water Discharge Data"] = (re.split(r"\bmgd\b", s, flags = re.IGNORECASE)[0].strip())
+                elif 'mgpd' in project_info[17][1].lower():
+                    s = project_info[17][1].lower()
+                    dri_post_processing_df.loc[dri, "Cleaned Water Discharge Data"] = (re.split(r"\bmgpd\b", s, flags = re.IGNORECASE)[0].strip())
+                else:
+                    dri_post_processing_df.loc[dri, "Cleaned Water Discharge Data"] = project_info[17][1].lower()
+                    '''
+                if 'gpd' in project_info[13][1].lower():
+                    s = project_info[13][1].lower()
+                    data_center_df.loc[dri, "Water Usage"] = (re.split(r"\bgpd\b", s, flags = re.IGNORECASE)[0].strip())/1000000
+                elif 'gallons per day' in project_info[13][1].lower():
+                    s = project_info[13][1].lower()
+                    data_center_df.loc[dri, "Water Usage"] = (re.split(r"\bgallons per day\b", s, flags = re.IGNORECASE)[0].strip())
+                    '''
+            except IndexError:
+                dri_post_processing_df.loc[dri, "Water Discharge Data"] = "No water data"
+                dri_post_processing_df.loc[dri, "Cleaned Water Discharge Data"] = "No water data"
+    #dri_post_processing_df['Cleaned Water Usage Data'] = dri_post_processing_df['Water Usage'].apply(parse_water_usage)
     dri_post_processing_output_path = dri_post_processing_path.parent / "dri_post_processing.csv"
+    dri_post_processing_df = dri_post_processing_df[["Water Usage", "Cleaned Water Usage Data", "Water Discharge Data", "Cleaned Water Discharge Data", "Contains 'data center'?", "Project Name", "Current Status", "Data Center?"]]
     dri_post_processing_df.to_csv(dri_post_processing_output_path, index = True)
     #print(dri_post_processing_df)
     print(dri_post_processing_df["Contains 'data center'?"].sum())
+    return dri_post_processing_df
 
 def parse_water_usage(value):
     '''
@@ -128,16 +159,18 @@ def parse_water_usage(value):
     except ValueError:
         return "No water data"
 
-water_data_post_processing()
+dri_post_processing_df = water_data_post_processing()
 
 #dri_post_processing_df = pd.read_csv("../Data/dri_post_processing.csv", index_col = "DRI Number")
 script_dir = Path(__file__).resolve().parent
 dri_post_processing_path = script_dir.parent / "Data" / "dri_post_processing.csv"
-dri_post_processing_df = pd.read_csv(dri_post_processing_path, index_col = "DRI Number")
+#dri_post_processing_df = pd.read_csv(dri_post_processing_path, index_col = "DRI Number")
 data_center_df = dri_post_processing_df[dri_post_processing_df["Contains 'data center'?"] == 1]
 
-data_center_df['Cleaned Water Usage Data'] = data_center_df['Water Usage'].apply(parse_water_usage)
-
+data_center_df.loc[:, 'Cleaned Water Usage Data'] = data_center_df['Water Usage'].apply(parse_water_usage)
+data_center_df.loc[:, 'Cleaned Water Discharge Data'] = data_center_df['Water Discharge Data'].apply(parse_water_usage)
+print(data_center_df.columns)
+data_center_df = data_center_df[["Water Usage", "Cleaned Water Usage Data", "Water Discharge Data", "Cleaned Water Discharge Data", "Contains 'data center'?", "Project Name", "Current Status", "Data Center?"]]
 print(data_center_df)
 data_center_output_path = dri_post_processing_path.parent / "data_center.csv"
 data_center_df.to_csv(data_center_output_path, index = True)
